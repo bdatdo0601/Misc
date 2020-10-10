aed5214c269855a8633095a45fd6ddfe27c81f28
root <root@storageboi.fios-router.home>
2020-10-10 13:45:00 -0400

    fix: update docker

--- packages/storageboi/docker-run.sh
+++ packages/storageboi/docker-run.sh
@@ -2,4 +2,5 @@
 docker run -d  --net=host --restart unless-stopped  \
        -e HASURA_GRAPHQL_DATABASE_URL=$POSTGRES_MAIN_DB_URL \
        -e HASURA_GRAPHQL_ENABLE_CONSOLE=true \
+       -e HASURA_GRAPHQL_ADMIN_SECRET=$HASURA_ADMIN_SECRET \
        hasura/graphql-engine:v1.3.2
