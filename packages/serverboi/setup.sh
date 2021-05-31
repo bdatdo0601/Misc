52da89367fe8f24215ebe9f49a92f662631ae9cd
Dat Do <dat.b.do@gmail.com>
2021-05-31 22:38:38 +0000

    fix: add setup

--- packages/serverboi/setup.sh
+++ packages/serverboi/setup.sh
@@ -1,22 +1,8 @@
 #!/bin/bash
 
-sudo snap install microk8s --classic
-sudo microk8s status --wait-ready
-sudo microk8s enable prometheus dashboard dns registry
+git clone https://github.com/openfaas/faasd --depth=1
+cd faasd
 
-sudo usermod -a -G microk8s bdatdo0601
-sudo chown -f -R bdatdo0601 ~/.kube
+./hack/install.sh
 
-sudo microk8s kubectl config view --raw > /root/.kube/config 
-
-curl -SLsf https://dl.get-arkade.dev/ | sudo sh
-arkade install openfaas --load-balancer
-
-curl -SLsf https://cli.openfaas.com | sudo sh
-
-sudo kubectl rollout status -n openfaas deploy/gateway
-sudo kubectl port-forward -n openfaas svc/gateway 8080:8080
-
-export OPENFAAS_URL=http://127.0.0.1:30915 >> ~/.zshrc
-
-#  PASSWORD=$(kubectl get secret -n openfaas basic-auth -o jsonpath="{.data.basic-auth-password}" | base64 --decode; echo)
+# sudo cat /var/lib/faasd/secrets/basic-auth-password | faas-cli login -s
